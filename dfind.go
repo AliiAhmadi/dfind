df1288040f783f52c0d771f21f9805921bb54f50
AliiAhmadi <aliahmadi.github@gmail.com>
2024-04-22 17:10:21 +0330

    + get path flag + if path not specified use current directory for path

--- dfind.go
+++ dfind.go
@@ -1,1 +1,20 @@
 package main
+
+import (
+	"flag"
+	"fmt"
+	"os"
+)
+
+func main() {
+	var path string
+	current, err := os.Getwd()
+	if err != nil {
+		panic(err)
+	}
+
+	flag.StringVar(&path, "path", current, "the root path that want to start scan and find duplicated files")
+	flag.Parse()
+
+	fmt.Println(path)
+}
