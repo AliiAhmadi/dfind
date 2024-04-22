7aa07daa2a330f1b77600ce2d22664d39ee21405
AliiAhmadi <aliahmadi.github@gmail.com>
2024-04-22 18:48:02 +0330

    + add unique function

--- Makefile
+++ Makefile
@@ -1,4 +1,4 @@
 .DEFAULT_GOAL := build
 
 build:
-	@go build -o dfind .
+	@go build -o dfind ./cmd/
--- dfind.go
+++ dfind.go
@@ -1,10 +1,8 @@
 package main
 
 import (
-	"flag"
 	"fmt"
 	"io/fs"
-	"log"
 	"os"
 	"path/filepath"
 )
@@ -12,37 +10,69 @@ import (
 type File struct {
 	info fs.FileInfo
 	path string
+	hash string
+}
+
+func ForceExit(f *os.File, msg string, status int) {
+	fmt.Fprint(f, msg)
+	os.Exit(status)
+}
+
+func Unique[T comparable](values []T) []T {
+	res := make([]T, 0, len(values))
+	seen := make(map[T]struct{}, len(values))
+
+	for _, item := range values {
+		if _, ok := seen[item]; ok {
+			continue
+		}
+
+		seen[item] = struct{}{}
+		res = append(res, item)
+	}
+
+	return res
 }
 
 func main() {
-	var path string
-	current, err := os.Getwd()
-	if err != nil {
-		panic(err)
-	}
-
-	flag.StringVar(&path, "path", current, "the root path that want to start scan and find duplicated files")
-	flag.Parse()
-
+	dirs := make([]string, 0)
 	files := make([]File, 0)
 
-	err = filepath.Walk(path, func(path string, info fs.FileInfo, err error) error {
+	args := Unique(os.Args[1:])
+
+	for _, dir := range args {
+		_, err := os.Open(dir)
 		if err != nil {
-			return err
+			ForceExit(os.Stderr, fmt.Sprintf("%v does not exist\n", dir), 1)
 		}
 
-		if !info.IsDir() {
-			files = append(files, File{
-				info: info,
-				path: path,
-			})
+		dirs = append(dirs, dir)
+	}
+
+	if len(args) == 0 {
+		wd, _ := os.Getwd()
+		dirs = append(dirs, wd)
+	}
+
+	for _, dir := range dirs {
+		err := filepath.Walk(dir, func(path string, info fs.FileInfo, err error) error {
+			if err != nil {
+				return nil
+			}
+
+			if !info.IsDir() {
+				files = append(files, File{
+					info: info,
+					path: path,
+					hash: "",
+				})
+			}
+			return nil
+		})
+
+		if err != nil {
+			ForceExit(os.Stderr, fmt.Sprintf("%v\n", err.Error()), 2)
 		}
-
-		return nil
-	})
-
-	if err != nil {
-		log.Fatal(err)
 	}
 
 	fmt.Println(files)
